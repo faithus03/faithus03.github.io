53dbcc8c4611b37cc1662ce41830c5a4e3a19760
faithus03 <38415710+faithus03@users.noreply.github.com>
2026-09-13 08:25:49 +0900

    Create start.js

---
+++ api/tesla/oauth/start.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.TESLA_CLIENT_ID,
+    redirect_uri: process.env.TESLA_REDIRECT_URI,
+    response_type: 'code',
+    scope: 'openid offline_access vehicle_location',
+    state: crypto.randomUUID()
+  });
+  res.writeHead(302, { Location: 'https://fleet-auth.prd.vn.cloud.tesla.com/oauth2/v3/authorize?' + params.toString() });
+  res.end();
+}
